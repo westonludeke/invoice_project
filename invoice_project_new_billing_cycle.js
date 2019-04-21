52ac33d30517f09f610cac9b9e987ebe69577e50
Weston Ludeke <westonludeke@gmail.com>
2019-04-21 14:34:14 -0500

    Uploaded New Billing Cycle
    Uploaded the JS code for New Billing Cycle

---
+++ invoice_project_new_billing_cycle.js
@@ -0,0 +1,71 @@
+//**All sensitive data below replaced by "xxxxxxxxx"
+
+//**You must be on the "Raw API Data" tab in the Sheet as the active tab before running the code**
+
+function onOpen() {
+  var ui = SpreadsheetApp.getUi();
+  ui.createMenu('Stripe')
+    .addItem('Retrieve Invoice Data','getInvoiceObj')
+    .addToUi();
+}
+
+function getInvoiceObj() 
+  {
+    var apiKey, content, options, response, secret, url;
+    
+    //**Stripe Test Keys / For Testing Purposes Only**
+    //secret = "sk_test_xxxxxxxxxxxxxxxxxxxxxxxxxx";
+    //apiKey = "xxxxxxxxxxxxxxxxxxxxxxxxxx";
+    
+    //**Live Key**
+    secret = "rk_live_xxxxxxxxxxxxxxxxxxxxxxxxxx";
+    apiKey = "xxxxxxxxxxxxxxxxxxxxxxxxxx";
+    
+    //Stripe API invoice URL ***only update the trailing code after /invoices/***
+    url = "https://api.stripe.com/v1/invoices/xxxxxxxxxxxxxxxxxxxxxxxxxx";
+    
+    //Direct Stipe dashboard URL to access a test invoice: https://dashboard.stripe.com/test/invoices/xxxxxxxxxxxxxxxxxxxxxxxxxx
+    
+    options = {
+      "method" : "GET",
+      "headers": {
+        "Authorization": "Bearer " + secret 
+      },
+      "muteHttpExceptions":true
+    };
+    
+    response = UrlFetchApp.fetch(url, options);
+    
+    //Push data to Sheet from invoice. **Writes over existing Sheet data**
+    content = JSON.parse(response.getContentText());
+    var sheet = SpreadsheetApp.getActiveSheet();
+   
+    //Retrieves currency type and adds to Sheet
+    sheet.getRange(5,2).setValue([content.currency.toUpperCase()]);
+    
+    //Invoice amount due
+    sheet.getRange(3,2).setValue([content.amount_due]);
+    
+    //Invoice date
+    sheet.getRange(1,2).setValue([content.date]);
+    
+    //Invoice period begin
+    sheet.getRange(6,2).setValue([content.lines.data[0].period.start]);
+    
+    //Invoice period end
+    sheet.getRange(7,2).setValue([content.lines.data[0].period.end]);
+    
+    //Invoice period end / Only for middle of billing cycle
+    //sheet.getRange(11,2).setValue([content.period_end]);
+    
+    //Number of licenses
+    sheet.getRange(10,2).setValue([content.lines.data[0].quantity]);
+    
+    //Invoice number
+    sheet.getRange(13,2).setValue([content.number]);
+    
+    //**Logs for testing**
+    //Logger.log("Yes, this was logged!");
+    Logger.log(response);
+    //Logger.log(content);
+}
