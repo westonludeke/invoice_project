5bcdc9b8fff3009277ea5aac536b0096e3efc4ac
Weston Ludeke <westonludeke@gmail.com>
2019-04-29 10:59:04 -0500

    Update invoice_project_new_billing_cycle.js
    Rewote a decent amount of the code to include an expansion of the Charge object from Stripe. Added in data from the payer's email address, credit card info, discount amount, etc.

--- invoice_project_new_billing_cycle.js
+++ invoice_project_new_billing_cycle.js
@@ -1,6 +1,13 @@
-//**All sensitive data below replaced by "xxxxxxxxx"
+/* HOW TO USE
 
-//**You must be on the "Raw API Data" tab in the Sheet as the active tab before running the code**
+1. Update the URL from the Stripe API with the information from the unique invoice URL
+2. Save the code
+3. Make sure "getInvoiceObj" is selected in the toolbar above
+4. Go to the Google Sheet and make sure you're on the "Raw API Data" tab in the Sheet as the active tab
+5. To run, either click "Stripe" > "Retreive Invoice Data" from the top of the Google Sheet.
+   Or you can click the play icon from the scripts page.
+
+*/
 
 function onOpen() {
   var ui = SpreadsheetApp.getUi();
@@ -21,10 +28,25 @@ function getInvoiceObj()
     secret = "rk_live_xxxxxxxxxxxxxxxxxxxxxxxxxx";
     apiKey = "xxxxxxxxxxxxxxxxxxxxxxxxxx";
     
-    //Stripe API invoice URL ***only update the trailing code after /invoices/***
-    url = "https://api.stripe.com/v1/invoices/xxxxxxxxxxxxxxxxxxxxxxxxxx";
+    /* ~~~~~~~IMPORTANT~~~~~~
     
-    //Direct Stipe dashboard URL to access a test invoice: https://dashboard.stripe.com/test/invoices/xxxxxxxxxxxxxxxxxxxxxxxxxx
+    With the invoice API URL below, only update the code of the key after /invoices/ and before ?expand[]=charge&expand[]=customer";   
+    
+    You will replace the unique invoice ID, for example "in_xxxxxxxxxxxxxxxxxxxxxxxxxx", with the ID of the invoice you want to pull.
+    
+    ~~~~~~~IMPORTANT~~~~~~*/
+    url = "https://api.stripe.com/v1/invoices/in_xxxxxxxxxxxxxxxxxxxxxxxxxx?expand[]=charge&expand[]=customer";
+    
+    
+    /* The URL below is for the Invoice Object only:
+      //url = "https://api.stripe.com/v1/invoices/in_xxxxxxxxxxxxxxxxxxxxxxxxxx";
+   */
+    
+    /* The URL below is for the Charge Object only:
+      //url = "https://api.stripe.com/v1/charges/ch_xxxxxxxxxxxxxxxxxxxxxxxxxx"
+    */
+    
+    //Direct Stipe dashboard URL to access a test invoice: https://dashboard.stripe.com/test/invoices/in_xxxxxxxxxxxxxxxxxxxxxxxxxx
     
     options = {
       "method" : "GET",
@@ -64,8 +86,31 @@ function getInvoiceObj()
     //Invoice number
     sheet.getRange(13,2).setValue([content.number]);
     
+    //Payer's email address
+    sheet.getRange(14,2).setValue([content.charge.card.name]);
+    
+    //Credit card type
+    sheet.getRange(15,2).setValue([content.charge.card.brand]);
+    
+    //Credit card last four
+    sheet.getRange(16,2).setValue([content.charge.card.last4]);
+    
+    //Credit card expiry month
+    sheet.getRange(17,2).setValue([content.charge.card.exp_month]);
+    
+    //Credit card expiry year
+    sheet.getRange(18,2).setValue([content.charge.card.exp_year]);
+    
+    //Team discount
+    sheet.getRange(21,2).setValue([content.customer.subscriptions.data[0].discount.coupon.percent_off]);
+    
+    //Is the invoice paid?
+    sheet.getRange(23,2).setValue([content.charge.paid]);
+    
     //**Logs for testing**
     //Logger.log("Yes, this was logged!");
     Logger.log(response);
     //Logger.log(content);
 }
+                        
+
